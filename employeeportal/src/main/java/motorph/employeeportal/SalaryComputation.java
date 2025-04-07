687d0a8b688814e17ac428dd264ac0a909cd1028
Hanna Ignacio <lr.hmignacio@mmdc.mcl.edu.ph>
2025-04-07 21:57:55 +0800

    revision of salary computation
    usage of attendance class into the logic of salary computation (gross and pay)

--- employeeportal/src/main/java/motorph/employeeportal/SalaryComputation.java
+++ employeeportal/src/main/java/motorph/employeeportal/SalaryComputation.java
@@ -25,24 +25,10 @@ import static java.lang.Integer.parseInt;
 */
 
 
-//class to handle the object/model
-class HoursMinutes {
-
-    long hoursWorked;
-    long minsWorked;
-    long overtimeHours;
-    long overtimeMins;
-
-    public HoursMinutes(long hoursWorked, long minsWorked, long overtimeHours, long overtimeMins) {
-        this.hoursWorked = hoursWorked;
-        this.minsWorked = minsWorked;
-        this.overtimeHours = overtimeHours;
-        this.overtimeMins = overtimeMins;
-    }
-
-}
-
 public class SalaryComputation {
+    
+    private static final Attendance attendance = new Attendance();
+    
     private static List<String[]> readCSVFromURL(String urlString) {
         List<String[]> data = new ArrayList<>();
         try (BufferedReader br = new BufferedReader(new InputStreamReader(new URL(urlString).openStream()));
@@ -53,68 +39,19 @@ public class SalaryComputation {
         return data;
     }
     
-    //attendance collector from csv per month
-    public static List<String[]> AttendanceCollector(String employeeNumber){
-        String csvURL = "https://drive.google.com/uc?export=download&id=1Gh7C6XjNXvdYJHEnS39kXN21CtkL-1Zh";
-        
-        long monthInput;
-        
-        //this is the scanner that will check for the user's input of month choice, it must be int
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the month you want to get the attendance of employee: " + employeeNumber);
-        System.out.println("6 -> June");
-        System.out.println("7 -> July");
-        System.out.println("8 -> August");
-        System.out.println("9 -> September");
-        System.out.println("10 -> October");
-        System.out.println("11 -> November");
-        System.out.println("12 -> December");
-        
-        System.out.print("Enter the month: ");
-        monthInput = scanner.nextInt();
-        
-        List<String[]> attendance = new ArrayList<>();
-        
-        System.out.println("Attendance Collector");
-        
-        List<String[]> rows = readCSVFromURL(csvURL); //Added as per URL reading
-
-           /* 
-           * Removed this part of the code to implement URL reading
-           * try(CSVReader csvReader = new CSVReader(new FileReader(csvFile));){
-           * 
-           *     List<String[]> rows = csvReader.readAll();
-           */     
-                //DateTime formatter will serve as the checker to check for the month
-                //Dynamic way of formatting the date pattern
-                DateTimeFormatter parserFormat = new DateTimeFormatterBuilder()
-                        .appendPattern("M/d/yyyy").
-                        toFormatter(Locale.ENGLISH);
-                for(int i = 1; i < rows.size(); i++){
-                    String[]value = rows.get(i);
-                    LocalDate localDate = LocalDate.parse(value[3], parserFormat);
-                    //if condition to chek whether the current value has the same month and entered employee number
-                    if(localDate.getMonthValue() == monthInput && parseInt(value[0]) == parseInt(employeeNumber)) {
-                                 attendance.add(value);
-                        } 
-                    } 
-                    return attendance;
-    };
     
     //calculation for gross pay
-    public static double GrossPay(String employeeNumber, long hours, long mins, long overtimeHours, long overtimeMins){ 
-                String csvURL = "https://drive.google.com/uc?export=download&id=1lQMufI6JKpVuEsQSBnbc9RkXdnfbGi2T";
+    public static double GrossPay(String employeeNumber, double minutesWorked, double overtimeMins){ 
+                String csvURL = "https://drive.google.com/uc?export=download&id=1Gh7C6XjNXvdYJHEnS39kXN21CtkL-1Zh";
         
         //declaration of the variables to use
         double hourlyRate = 0.0;
         double salary = 0.0;
         double riceSubsidy = 0.0;
         double phoneAllowance = 0.0;
-        double overtimeRate = 0.0;
+        double overtimeRate = 0.0 / 60;
         
-        
-        System.out.println("Gross Pay Collector");
-           
+    
             /*try(CSVReader csvReader = new CSVReader(new FileReader(csvFile));){
             *    List<String[]> rows = csvReader.readAll();
             */
@@ -124,85 +61,29 @@ public class SalaryComputation {
             for(int i = 1; i < rows.size(); i++){
                 String[]value = rows.get(i);
                 if(parseInt(value[0]) == parseInt(employeeNumber)){
-                    hourlyRate = parseFloat(value[18]);
+                    hourlyRate = parseFloat(value[18]) / 60;
                     //add subsidies
                     //replace method is used to negate or escape the comma -> 1,500
                     riceSubsidy = parseFloat(value[14].replace(",", ""));
                     phoneAllowance = parseInt(value[15].replace(",", ""));
                     //overtime calculation based on labor code
-                    overtimeRate = hourlyRate * 1.25;
+                    overtimeRate = (hourlyRate * 1.25) / 60;
                     break;
                 }
             }
             
-            double minuteRate = hourlyRate / 60; 
-            double minuteOvertimeRate = overtimeRate / 60;
-            salary = (hourlyRate * hours) + (minuteRate * mins) + riceSubsidy + phoneAllowance + (overtimeRate * overtimeHours) + (minuteOvertimeRate * overtimeMins);
+            salary = (hourlyRate * minutesWorked) + riceSubsidy + phoneAllowance + (overtimeRate * overtimeMins);
+            
                      
         return salary;
     };
     
     
-    //ATTENDANCE COMPUTATION
-    public static HoursMinutes HoursCalculator(List<String[]> employee){
-        
-        long hoursGathered = 0;
-        long minsGathered = 0;
-        long overtimeHours = 0;
-        long overtimeMins = 0;
-        
-        for(int i = 0; i < employee.size(); i++){
-                String[]empDetails = employee.get(i);
-                
-                String[] splitStringStart = empDetails[4].split(":"); //8:00 -> splitStringStart =["8", "00"]
-                String[] splitStringEnd = empDetails[5].split(":");// 16:31 -> 16 31
-
-                //declaration of variables to get hours and minutes from splitted String
-                int hoursStartTime = Integer.parseInt(splitStringStart[0]);
-                int minsStartTime = Integer.parseInt(splitStringStart[1]);
-
-                //declaration of variables to get hours and minutes from splitted String
-                int hoursEndTime = Integer.parseInt(splitStringEnd[0]);
-                int minsEndTime = Integer.parseInt(splitStringEnd[1]);
-
-                /* by using LocalTime library, 
-                we will transpose the data type of string to LocalTime 
-                of the two entered time entries of the user*/
-
-                LocalTime loginTime = LocalTime.of(hoursStartTime, minsStartTime);
-                LocalTime logoutTime = LocalTime.of(hoursEndTime, minsEndTime);
-
-                //calculation of the duration of the two time entries
-                Duration duration = Duration.between(loginTime, logoutTime);
-                
-                long minsWorked = duration.toMinutes();
-                minsWorked -= 60;
-                
-                long hoursWorked = minsWorked / 60;
-                long minutesWorked = minsWorked % 60;
-
-                //minus 1 for lunch break
-                hoursGathered += hoursWorked;
-                minsGathered += minutesWorked;
-                
-                
-                if(hoursWorked > 8){
-                    overtimeHours += (hoursWorked - 8);
-                    overtimeMins += minutesWorked;
-                }
-                else if(hoursWorked == 8 && minutesWorked > 0){
-                    overtimeMins += minutesWorked;
-                }
-                    
-            }
-                overtimeHours = overtimeMins / 60;
-                overtimeMins = overtimeMins % 60;
-     
-        return new HoursMinutes(hoursGathered, minsGathered, overtimeHours, overtimeMins);
-    };
-
+  
     // COMPUTE SALARY DEDUCTIONS
-    public static double computeDeductions(double salary) {
+    public static double computeDeductions(double salary, String employeeId, double late) {
+        
+    String csvURL = "https://drive.google.com/uc?export=download&id=1Gh7C6XjNXvdYJHEnS39kXN21CtkL-1Zh";
     double[][] sssRanges = {
         {0, 3249.99, 135.0}, {3250, 3749.99, 157.5}, {3750, 4249.99, 180.0},
         {4250, 4749.99, 202.5}, {4750, 5249.99, 225.0}, {5250, 5749.99, 247.5},
@@ -220,9 +101,22 @@ public class SalaryComputation {
             break;
         }
     }
+    
+    List<String[]> rows = readCSVFromURL(csvURL);
+    
+    double lateRate = 0.0;
+
+            for(int i = 1; i < rows.size(); i++){
+                String[]value = rows.get(i);
+                if(parseInt(value[0]) == parseInt(employeeId)){
+                    lateRate = parseFloat(value[18]) / 60;
+                    break;
+                }
+            }
 
     double philHealthDeduction = (salary >= 10000 && salary <= 100000) ? (salary * 0.05) / 2 : 0;
     double pagIbigDeduction = 100.0;
+    double lateDeductions = lateRate * late;
 
     double taxableIncome = salary - (sssDeduction + philHealthDeduction + pagIbigDeduction);
     double withholdingTax = 0;
@@ -239,67 +133,89 @@ public class SalaryComputation {
         withholdingTax = 200833 + (taxableIncome - 666667) * 0.35;
     }
 
-    double totalDeductions = sssDeduction + philHealthDeduction + pagIbigDeduction + withholdingTax;
+    double totalDeductions = sssDeduction + philHealthDeduction + pagIbigDeduction + withholdingTax + lateDeductions;
     double netSalary = salary - totalDeductions;
 
     System.out.println("\n--- Deductions Breakdown ---");
     System.out.println("SSS Deduction: PHP " + sssDeduction);
     System.out.println("PhilHealth Deduction: PHP " + philHealthDeduction);
     System.out.println("Pag-IBIG Deduction: PHP " + pagIbigDeduction);
-    System.out.println("Withholding Tax: PHP " + withholdingTax);
-    System.out.println("Total Deductions: PHP " + totalDeductions);
+    System.out.printf("Withholding Tax: PHP %.2f\n ", withholdingTax);
+    System.out.printf("Late Deductions: PHP %.2f\n ", lateDeductions);
+    System.out.printf("Total Deductions: PHP %.2f\n ", totalDeductions);
+    System.out.println("\n--- End of Deductions Breakdown ---");
+    System.out.println("");
 
     return netSalary;
  }
     
     //METHOD FOR ATTENDANCE RECORD + SALARY COMPUTATION
     public void computeSalary(String employeeId) {
-      /*  
-        *Scanner scanner = new Scanner(System.in);
-        *
-        *String employeeNumber;
-        *System.out.println("");
-        *System.out.print("Enter Employee Number: ");
-        *employeeNumber = scanner.nextLine();
-        *Alexaire - this is okay to remove once it is compiled with the main controller
-        */
         
-        
-        //when you compile this, kindly add or attach the employee number entered in the main controller in order for the methods to run
-        System.out.println("|-- Attendance Record of Employee " + employeeId + " --|");
+        //System.out.println("|-- Attendance Record of Employee " + employeeId + " --|");
         System.out.println("");
         
-        List<String[]> attendance = AttendanceCollector(employeeId);
-        for(int i = 1; i < attendance.size(); i++){
-                String[]empDetails = attendance.get(i);
-                for (String value : empDetails) {
-                    System.out.print(value + "\t");  // Print each value separated by a tab
-                }
-                System.out.println();
+//        List<String[]> attendance = AttendanceCollector(employeeId);
+//        for(int i = 1; i < attendance.size(); i++){
+//                String[]empDetails = attendance.get(i);
+//                for (String value : empDetails) {
+//                    System.out.print(value + "\t");  // Print each value separated by a tab
+//                }
+//                System.out.println();
+//            }
+
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Enter year (e.g. 2024): ");
+            int year = scanner.nextInt();
+            scanner.nextLine();
+
+            while (true) { 
+            // Display months from (1 to 12)
+            System.out.println("\nSelect a month:");
+            for (int i = 1; i <= 12; i++) {
+                System.out.println(i + ". " + Month.of(i));
             }
-    
-        
-        HoursMinutes calculatedHours = HoursCalculator(attendance);
-        long minsToHours = calculatedHours.minsWorked / 60;
-        long hoursGathered = calculatedHours.hoursWorked + minsToHours;
-        long excessMinutes = minsToHours % 60;
-        long overtimeHoursGathered = calculatedHours.overtimeHours;
-        long excessOvertimeMinutes = calculatedHours.overtimeMins;
+            System.out.println("13. Back to Main Menu");
+            System.out.print("Choose month: ");
+            int monthChoice = scanner.nextInt();
+            scanner.nextLine();
 
-        
-        System.out.println("");
-        System.out.println("Hours worked: " + hoursGathered + " hours & " + excessMinutes + " minutes.");
-        System.out.println("Overtime worked " + overtimeHoursGathered + " hours & " + excessOvertimeMinutes + " minutes.");
-        System.out.println("");
-      
+            // Added Choice 13, to return to main menu
+            // Choice of int (1 to 12); month also displayed for convenience
+            // Retry if Choice is not int 1 to 12 or 13 (e.g. 0, 14-99)
+            if (monthChoice == 13) break;
+            if (monthChoice < 1 || monthChoice > 12) {
+                System.out.println("Invalid! Please Try again.");
+            }
+            ArrayList<ArrayList<Double>> attendanceCollector = attendance.displayAttendance(employeeId, year, monthChoice);
+            System.out.println("");
+            
+            double totalGross = 0.0;
+            double totalLate = 0.0;
+            double netPay = 0.0;
+            
+            for (int i = 0; i < attendanceCollector.size(); i++) {
+                    
+                    ArrayList<Double> weekData = attendanceCollector.get(i);
 
-        double grossSalary = GrossPay(employeeId, hoursGathered, excessMinutes, overtimeHoursGathered, excessOvertimeMinutes);
-            System.out.println("\nGross Salary of the employee for the month: PHP " + grossSalary);
-             // Call deduction calculation
-             double netSalary = computeDeductions(grossSalary);
-         
-            // DISPLAY NET SALARY
-            System.out.println("\nNet Salary: PHP " + netSalary);
-        
+                    if (weekData.size() >= 3) {
+                        double work = weekData.get(0);
+                        double late = weekData.get(1);
+                        double ot   = weekData.get(2);
+
+                        double grossPay = GrossPay(employeeId, work, ot);
+                        System.out.printf("Gross Pay for Week %d : PHP %.2f\n", i + 1, grossPay);
+                        totalGross += grossPay;
+                        totalLate += late;
+                        
+                    } else {
+                        System.out.println("⚠️ Skipping week due to incomplete data!");
+                    }
+                }
+            System.out.printf("Gross Pay for the month: PHP %.2f\n", totalGross);
+            netPay = computeDeductions(totalGross, employeeId, totalLate);
+            System.out.printf("Net Pay for the month: PHP %.2f\n", netPay);
+        }
+     
     }
 }
